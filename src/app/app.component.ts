be81c3fd295eba8be5b03bba756143e1b9a92c73
Adriana Lizbeth  Linares Rayas <linaresrayas@gmail.com>
2020-03-25 13:09:17 -0600

    Map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Marker } from './marker.model';
 
 @Component({
   selector: 'app-root',
@@ -6,8 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  marker: Marker;
+  markers: Marker[]
+
+  //Eliminar
   lat = null;
   lng = null;
   googleMapKey = 'AIzaSyA5mjCwx1TRLuBAjwQw84WE6h5ErSe7Uj8';
   title = 'covid19';
+
+  addMarker() {
+    this.marker = new Marker (20.6752255, -103.3612204);
+    this.markers.push(this.marker);
+    console.log('info', this.marker);
+  }
+  
 }
